ec763694b088cf9159e61377d444e64f44a099ec
Madan Parameswaran <madan.mesh@gmail.com>
2016-02-19 17:15:00 +1030

    UPDATE web.xml

--- OldIsGoldRITS/src/java/oldisgoldrits/properties/DatabaseConnector.java
+++ OldIsGoldRITS/src/java/oldisgoldrits/properties/DatabaseConnector.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,23 +17,27 @@ import java.util.Properties;
  */
 public class DatabaseConnector {
     
-    public Connection connect()
+    public void connect()
     {
         Properties prop = new Properties();
     	InputStream input = null;
     	
     	try {
-        
-    		String filename = "config.properties";
-    		
+         
+    		String filename = "/config.properties";
+    		input = getClass().getClassLoader().getResourceAsStream(filename);
+    		if(input==null){
+    	            System.out.println("Sorry, unable to find " + filename);
+    		  //  return null;
+    		}
 
     		//load a properties file from class path, inside static method
     		prop.load(input);
- 
+                Logger log = Logger.getLogger(getClass().getSimpleName());
                 //get the property value and print it out
-                System.out.println(prop.getProperty("database"));
-    	        System.out.println(prop.getProperty("dbuser"));
-    	        System.out.println(prop.getProperty("dbpassword"));
+                log.info(prop.getProperty("database"));
+    	        log.info(prop.getProperty("user"));
+    	        log.info(prop.getProperty("pass"));
  
     	} catch (IOException ex) {
     		ex.printStackTrace();
