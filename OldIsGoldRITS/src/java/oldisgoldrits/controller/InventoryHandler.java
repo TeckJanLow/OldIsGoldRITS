433b61a866cae343846a82641e87bba49f9e8e56
TeckJanLow <teckjanlow@gmail.com>
2016-02-20 19:12:58 +1030

    Modified inventory queries and added javadocs for handlers

--- OldIsGoldRITS/src/java/oldisgoldrits/controller/InventoryHandler.java
+++ OldIsGoldRITS/src/java/oldisgoldrits/controller/InventoryHandler.java
@@ -13,15 +13,34 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
+ * Contains all methods involving the creation and manipulation of Inventory items.
  * @author Teck Jan Low
+ * @version 1.0
  */
 public class InventoryHandler {
 
     /**
-     * 
-     * @param condition
-     * @return
+     * Retrieves a ResultSet of all inventory information.
+     * @return A ResultSet of all inventory data
+     * @throws SQLException 
+     */
+    public ResultSet getInventory() throws SQLException {
+        
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.connect();
+        String query = "SELECT INVENTORY.*, ALBUM.title, ALBUM.artist, "
+                + "ALBUM.genre, ALBUM.comments FROM INVENTORY LEFT JOIN ALBUM "
+                + "ON INVENTORY.album_id = ALBUM.album_id";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        conn.close();
+        return rs;
+    }
+    
+    /**
+     * Retrieves a ResultSet of inventory information from the database with conditions.
+     * @param condition Specified conditions for data retrieval
+     * @return A ResultSet containing retrieved data
      * @throws SQLException 
      */
     public ResultSet getInventory(String condition) throws SQLException {
@@ -30,10 +49,7 @@ public class InventoryHandler {
         Connection conn = dbc.connect();
         String query = "SELECT INVENTORY.*, ALBUM.title, ALBUM.artist, "
                 + "ALBUM.genre, ALBUM.comments FROM INVENTORY LEFT JOIN ALBUM "
-                + "ON INVENTORY.album_id = ALBUM.album_id";
-        if (!condition.isEmpty()) {
-            query += "WHERE " + condition;
-        }
+                + "ON INVENTORY.album_id = ALBUM.album_id WHERE " + condition;
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery(query);
         conn.close();
@@ -41,36 +57,54 @@ public class InventoryHandler {
     }
     
     /**
-     * 
-     * @param quality
-     * @param quantityOnHand
-     * @param price
+     * Adds a new inventory item to the database.
+     * @param quality The quality of the item
+     * @param quantityOnHand The quantity of items on hand
+     * @param price The retail price of the item
+     * @param purchasePrice The cost per unit of the item
      * @throws SQLException 
      */
-    public void addNewInventory(String quality, int quantityOnHand, long price) 
-            throws SQLException {
+    public void addNewInventory(String quality, int quantityOnHand, long price, 
+            long purchasePrice) throws SQLException {
         
         DatabaseConnector dbc = new DatabaseConnector();
         Connection conn = dbc.connect();
-        String query = "INSERT INTO INVENTORY (quality, quantity, price) VALUES"
-                + " (" + quality + ", " + quantityOnHand + ", " + price + ")";
+        String query = "INSERT INTO INVENTORY (quality, quantity, price, "
+                + "purchase_price) VALUES (" + quality + ", " + quantityOnHand 
+                + ", " + price + ", " + purchasePrice + ")";
         Statement st = conn.createStatement();
         st.executeUpdate(query);
         conn.close();
     }
     
+    /**
+     * Edits an inventory item on the database
+     * @param inventoryID The ID of the inventory item to be edited
+     * @param quality The new quality of the item
+     * @param quantity The new quantity of the item
+     * @param price The new price of the item
+     * @param purchasePrice The new purchase price of the item
+     * @throws SQLException 
+     */
     public void editInventory(int inventoryID, String quality, String quantity, 
-            long price) throws SQLException {
+            long price, long purchasePrice) throws SQLException {
         
         DatabaseConnector dbc = new DatabaseConnector();
         Connection conn = dbc.connect();
         String query = "UPDATE INVENTORY SET quality = '" + quality + "', "
-                + "quantity = '" + quantity + "', price = '" + price + "'";
+                + "quantity = '" + quantity + "', price = '" + price + "', "
+                + "purchase_price = '" + purchasePrice + "' WHERE sku = '"
+                + inventoryID + "'";
         Statement st = conn.createStatement();
         st.executeUpdate(query);
         conn.close();
     }
     
+    /**
+     * Deletes an inventory item from the database.
+     * @param inventoryID The ID of the inventory item to be deleted
+     * @throws SQLException 
+     */
     public void deleteInventory(int inventoryID) throws SQLException {
         
         DatabaseConnector dbc = new DatabaseConnector();
--- OldIsGoldRITS/src/java/oldisgoldrits/controller/RequestHandler.java
+++ OldIsGoldRITS/src/java/oldisgoldrits/controller/RequestHandler.java
@@ -13,14 +13,35 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
+ * Contains all methods involving the creation and manipulation of customer requests.
  * @author Teck Jan Low
+ * @version 1.0
  */
 public class RequestHandler {
     
     /**
-     * This method queries the database for information regarding requests and 
-     * their associated customers and employees.
+     * Retrieves all request information from the database.
+     * @return ResultSet containing all request information from the database
+     * @throws SQLException 
+     */
+    public ResultSet getRequest() throws SQLException {
+        
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.connect();
+        String query = "SELECT REQUEST.*, CUSTOMER.first_name cust_first, "
+                + "CUSTOMER.last_name cust_last, CUSTOMER.phone, CUSTOMER.email,"
+                + " CUSTOMER.preferred_mode, EMPLOYEE.first_name emp_first, "
+                + "EMPLOYEE.last_name emp_last FROM REQUEST LEFT JOIN EMPLOYEE "
+                + "ON REQUEST.emp_id = EMPLOYEE.emp_id LEFT JOIN CUSTOMER ON"
+                + "REQUEST.cust_id = CUSTOMER.cust_id";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        conn.close();
+        return rs;
+    }
+    
+    /**
+     * Retrieve information regarding requests and their associated customers and employees with a condition.
      * @param condition optional condition to reduce the number of results.
      * @return returns a ResultSet containing the data from the query.
      * @throws SQLException 
@@ -34,10 +55,7 @@ public class RequestHandler {
                 + " CUSTOMER.preferred_mode, EMPLOYEE.first_name emp_first, "
                 + "EMPLOYEE.last_name emp_last FROM REQUEST LEFT JOIN EMPLOYEE "
                 + "ON REQUEST.emp_id = EMPLOYEE.emp_id LEFT JOIN CUSTOMER ON"
-                + "REQUEST.cust_id = CUSTOMER.cust_id";
-        if (!condition.isEmpty()) {
-            query += "WHERE " + condition;
-        }
+                + "REQUEST.cust_id = CUSTOMER.cust_id WHERE " + condition;
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery(query);
         conn.close();
@@ -45,13 +63,13 @@ public class RequestHandler {
     }
 
     /**
-     * 
-     * @param customerID
-     * @param employeeID
-     * @param description
-     * @param date
-     * @param quantity
-     * @param isComplete
+     * Adds a new request into the database.
+     * @param customerID The request customer ID
+     * @param employeeID The employee ID associated with the request
+     * @param description The description of the request
+     * @param date The date the request was made
+     * @param quantity The quantity requested
+     * @param isComplete The fulfillment status of the request
      * @throws SQLException 
      */
     public void addNewRequest(int customerID, int employeeID, String description
@@ -69,11 +87,11 @@ public class RequestHandler {
     }
     
     /**
-     * 
-     * @param requestID
-     * @param description
-     * @param quantity
-     * @param isComplete
+     * Edits a request on the database.
+     * @param requestID The ID of the request to be edited
+     * @param description The new description of the request
+     * @param quantity The new quantity requested
+     * @param isComplete The new status of the request
      * @throws SQLException 
      */
     public void editRequest(int requestID, String description, int quantity, 
@@ -89,6 +107,11 @@ public class RequestHandler {
         conn.close();
     }
     
+    /**
+     * Deletes a request from the database.
+     * @param requestID The ID of the request to be deleted
+     * @throws SQLException 
+     */
     public void deleteRequest(int requestID) throws SQLException {
         
         DatabaseConnector dbc = new DatabaseConnector();
