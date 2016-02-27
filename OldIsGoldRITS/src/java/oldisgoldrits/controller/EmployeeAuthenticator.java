95d3972647f9bc33b2818f20cfb5534faa0a6966
Madan Parameswaran <madan.mesh@gmail.com>
2016-02-27 15:52:43 +1030

    ADD: authenticator

--- OldIsGoldRITS/src/java/oldisgoldrits/controller/EmployeeAuthenticator.java
+++ OldIsGoldRITS/src/java/oldisgoldrits/controller/EmployeeAuthenticator.java
@@ -6,8 +6,13 @@
 package oldisgoldrits.controller;
 
 import config.DatabaseConnector;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,14 +22,40 @@ public class EmployeeAuthenticator {
     
     public static String login(String user, String pass)
     {
-        DatabaseConnector dbc = new DatabaseConnector();
-        Connection conn = dbc.connect();
-        String query = "SELECT emp_id from EMPLOYEE WHERE email = '"+user+"' and ";
-//        Statement st = conn.createStatement();
-//        st.executeUpdate(query);
-//        conn.close();
-        
+        try {
+            DatabaseConnector dbc = new DatabaseConnector();
+            Integer result;
+            try (Connection conn = dbc.connect()) {
+                String salt = new EmployeeAuthenticator().saltShaker();
+                String query = "SELECT emp_id from EMPLOYEE WHERE email = '"+user+"' and pass=aes_encrypt('"+pass+"','"+salt+"')";
+                Statement st = conn.createStatement();
+                result = st.executeUpdate(query);
+            }
+            return result.toString();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeAuthenticator.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
+    }
+    
+    private String saltShaker()
+    {
+        Properties prop = new Properties();
+        String filename = "config.properties";
+        try{    
+            InputStream input = getClass().getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + filename);
+                  return null;
+            }
+        }
+        catch(Exception ex)
+        {
+            Logger.getLogger(getClass().getSimpleName()).severe(ex.getMessage());
+        }
+        
+         return prop.getProperty("partial_salt");
     }
     
 }
